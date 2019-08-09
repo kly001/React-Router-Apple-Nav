bea70de78c8148d89479e46d0d5245903ff95b3d
kly001 <karenly1961@gmail.com>
2019-08-09 16:57:47 -0400

    Category section links working

--- apple-nav/src/components/Home.js
+++ apple-nav/src/components/Home.js
@@ -6,27 +6,27 @@ const Home = () => {
         <div>
            <h2>Home Page</h2>
             <div className="product-section-main">
-                <Link to ="/mac"><h4>Mac</h4></Link>
+                <Link to ="/categories/3"><h4>Mac</h4></Link>
                 <p>Link to Mac main page</p>
             </div>
             <div className="product-section-main">
-                <Link to = "/ipad"><h4>iPad</h4></Link>
+                <Link to = "/categories/1"><h4>iPad</h4></Link>
                 <p>Link to iPad main page</p>
             </div>
             <div className="product-section-main">
-                <Link to= "/iphone"><h4>iPhone</h4></Link>
+                <Link to= "/categories/2"><h4>iPhone</h4></Link>
                 <p>Link to iPhone main page</p>
             </div>
              <div className="product-section-main">
-                <Link to= "watch"><h4>Watch</h4></Link>
+                <Link to= "/categories/6"><h4>Watch</h4></Link>
                 <p>Link to Watch main page</p>
             </div>
             <div className="product-section-main">
-                <Link to = "/tv"><h4>TV</h4></Link>
+                <Link to = "/categories/5"><h4>TV</h4></Link>
                 <p>Link to TV main page</p>
             </div>
             <div className="product-section-main">
-                <Link to = "/music"><h4>Music</h4></Link>
+                <Link to = "/categories/4"><h4>Music</h4></Link>
                 <p>Link to Music main page</p>
             </div>  
         </div>
