c6ec70d44d409831b52e9cbb28edc69423a17f24
kly001 <karenly1961@gmail.com>
2019-08-09 15:35:59 -0400

    Setup Navigation Bar

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink} from "react-router-dom";
 
 import './App.css';
 
@@ -6,6 +7,17 @@ function App() {
   return (
     <div className="App">
       <h1>React-Router-Apple-Nav Project</h1>
+      <div className="navbar">
+        <NavLink to= "/"> Home </NavLink>
+        <NavLink to= "/ipad"> iPad </NavLink>
+        <NavLink to= "/iphone"> iPhone </NavLink>
+        <NavLink to= "/mac"> Mac </NavLink>
+        <NavLink to= "/Music"> Music </NavLink>
+        <NavLink to= "/tv"> TV </NavLink>
+        <NavLink to= "/watch"> Watch </NavLink>
+        <NavLink to= "/support"> Support </NavLink>
+      </div>
+
     </div>
   );
 }
