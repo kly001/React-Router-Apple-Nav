62a2a651de9c3f5649008e00efdbe08731628dcc
kly001 <karenly1961@gmail.com>
2019-08-09 15:10:17 -0400

    Created data.js file

---
+++ apple-nav/src/components/data.js
@@ -0,0 +1,95 @@
+const categories = [
+    {
+        id:1,
+        name: "iPad",
+        description: "List of iPad products",
+        products: [
+            "iPadPro",
+            "iPadAir",
+            "iPad",
+            "iPad mini",
+            "Compare",
+            "Apple Pencil",
+            "Smart Keyboard",
+            "Accessories",
+            "iOS12",
+        ]
+    },
+    {
+        id:2,
+        name: "iPhone",
+        description: "List of iPhone products",
+        products: [
+            "iPhoneXS",
+            "iPhoneXR",
+            "iPhone8",
+            "iPhone7",
+            "Compare",
+            "AppleCard",
+            "AirPods",
+            "Accessories",
+            "iOS12",
+        ]
+    },
+    {
+        id:3,
+        name: "Mac",
+        description: "List of Mac products",
+        products: [
+            "MacBook Air",
+            "MacBook Pro",
+            "iMac",
+            "iMacPro",
+            "MacPro",
+            "Mac mini",
+            "Compare",
+            "ProDisplay XDR",
+            "Accessories",
+            "Mojave",
+        ]
+    },
+    {
+        id:4,
+        name: "Music",
+        description: "List of Music products",
+        products: [
+            "Apple Music",
+            "iTunes",
+            "HomePod",
+            "AirPods",
+            "iPod Touch",
+            "Music Accessories",
+            "Gift Cards",
+        ]
+    },
+    {
+        id:5,
+        name: "TV",
+        description: "List of TV products",
+        products: [
+            "Apple TV app",
+              "Apple TV+",
+              "Apple TV 4K",
+              "Apple TV HD",
+              "Air Play",
+              "Accessories",
+        ]
+    },
+    {
+        id:6,
+        name: "Watch",
+        description: "List of Watch products",
+        products: [
+            "Apple Watch Series 4",
+            "Apple Watch Nike+",
+            "Apple Watch Hermes",
+            "Apple Watch Series 3",
+            "Watch OS",
+            "Bands",
+            "Accessories",
+            "Compare",
+        ]
+    },
+
+]
+export default categories
