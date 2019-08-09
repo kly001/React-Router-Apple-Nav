0699acb2c8b6c2f75791c01a26256f997c33d891
kly001 <karenly1961@gmail.com>
2019-08-09 15:18:35 -0400

    Home.js setup

---
+++ apple-nav/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>React-Router-Apple-Nav Project</h1>
+            <Link to ="/categories">ENTER</Link>
+        </div>
+    )
+}
+export default Home
