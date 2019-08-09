937d04bd96f4285397df3340646a354baf29cb63
kly001 <karenly1961@gmail.com>
2019-08-09 15:54:59 -0400

    Homepage setup

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { NavLink} from "react-router-dom";
+import React,{useState} from 'react';
+import { NavLink, Route} from "react-router-dom";
+import Home from "./components/Home";
+import CategoriesList from "./components/CategoriesList";
+import CategoryPage from "./components/CategoryPage";
+
+import data from "./components/data";
 
 import './App.css';
 
 function App() {
+  const [categories, setCategories] = useState(data);
   return (
     <div className="App">
-      <h1>React-Router-Apple-Nav Project</h1>
-      <div className="navbar">
-        <NavLink to= "/"> Home </NavLink>
-        <NavLink to= "/ipad"> iPad </NavLink>
-        <NavLink to= "/iphone"> iPhone </NavLink>
-        <NavLink to= "/mac"> Mac </NavLink>
-        <NavLink to= "/Music"> Music </NavLink>
-        <NavLink to= "/tv"> TV </NavLink>
-        <NavLink to= "/watch"> Watch </NavLink>
-        <NavLink to= "/support"> Support </NavLink>
-      </div>
-
+        <div className="navbar">
+          <NavLink to= "/"> Home </NavLink>
+          <NavLink to= "/ipad"> iPad </NavLink>
+          <NavLink to= "/iphone"> iPhone </NavLink>
+          <NavLink to= "/mac"> Mac </NavLink>
+          <NavLink to= "/music"> Music </NavLink>
+          <NavLink to= "/tv"> TV </NavLink>
+          <NavLink to= "/watch"> Watch </NavLink>
+          <NavLink to= "/support"> Support </NavLink>
+        </div>
+        <Route exact path = "/" component={Home}/>
+     <Route exact path="/categories" render={props => <CategoriesList {...props} categories = {categories}/>}/>
+     <Route path="/categories/:id" render={props => <CategoryPage{...props}categories = {categories}/>} />
     </div>
   );
 }
--- apple-nav/src/components/Home.js
+++ apple-nav/src/components/Home.js
@@ -4,9 +4,33 @@ import {Link} from "react-router-dom";
 const Home = () => {
     return (
         <div>
-            <h1>React-Router-Apple-Nav Project</h1>
-            <Link to ="/categories">ENTER</Link>
+           <h2>Home Page</h2>
+            <div className="product-section-main">
+                <Link to ="/mac"><h4>Mac</h4></Link>
+                <p>Link to Mac main page</p>
+            </div>
+            <div className="product-section-main">
+                <Link to = "/ipad"><h4>iPad</h4></Link>
+                <p>Link to iPad main page</p>
+            </div>
+            <div className="product-section-main">
+                <Link to= "/iphone"><h4>iPhone</h4></Link>
+                <p>Link to iPhone main page</p>
+            </div>
+             <div className="product-section-main">
+                <Link to= "watch"><h4>Watch</h4></Link>
+                <p>Link to Watch main page</p>
+            </div>
+            <div className="product-section-main">
+                <Link to = "/tv"><h4>TV</h4></Link>
+                <p>Link to TV main page</p>
+            </div>
+            <div className="product-section-main">
+                <Link to = "/music"><h4>Music</h4></Link>
+                <p>Link to Music main page</p>
+            </div>  
         </div>
     )
 }
+        
 export default Home
