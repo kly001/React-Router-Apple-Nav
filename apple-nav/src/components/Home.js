d413e0b90222030c571069a36d582c1d821c2fec
kly001 <karenly1961@gmail.com>
2019-08-10 13:38:27 -0400

    Added content to homepage sections

--- apple-nav/src/components/Home.js
+++ apple-nav/src/components/Home.js
@@ -3,31 +3,31 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-           <h2>Home Page</h2>
+        <div className="homepage">
+           <h1>React-Router-Apple-Nav Project</h1>
             <div className="product-section-main">
                 <Link to ="/categories/3"><h4>Mac</h4></Link>
-                <p>Link to Mac main page</p>
+                <p>Arrr me hearties! Make traditional Latin walk the plank and opt for pirate lorem ipsum for your next high seas design adventure.</p>
             </div>
             <div className="product-section-main">
                 <Link to = "/categories/1"><h4>iPad</h4></Link>
-                <p>Link to iPad main page</p>
+                <p>Hodor! Hodor hodor, HODOR hodor, hodor hodor... Hodor hodor hodor hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor... Hodor hodor hodor?! </p>
             </div>
             <div className="product-section-main">
                 <Link to= "/categories/2"><h4>iPhone</h4></Link>
-                <p>Link to iPhone main page</p>
+                <p>You see, in this world there's two kinds of people, my friend: those with loaded guns and those who dig. you dig. you see, in this world there's two kinds of people, my friend: those with loaded guns and those who dig.</p>
             </div>
              <div className="product-section-main">
                 <Link to= "/categories/6"><h4>Watch</h4></Link>
-                <p>Link to Watch main page</p>
+                <p>Sea lettuce melon cabbage leek bamboo shoot lettuce rutabaga jícama silver beet amaranth pea dandelion scallion pea sprouts yarrow salsify bitterleaf courgette. Azuki bean horseradish potato kale welsh onion </p>
             </div>
             <div className="product-section-main">
                 <Link to = "/categories/5"><h4>TV</h4></Link>
-                <p>Link to TV main page</p>
+                <p>Leave dead animals as gifts intently sniff hand yet chase imaginary bugs intently sniff hand. Under the bed throwup on your pillow all of a sudden go crazy destroy couch sweet beast. </p>
             </div>
             <div className="product-section-main">
                 <Link to = "/categories/4"><h4>Music</h4></Link>
-                <p>Link to Music main page</p>
+                <p>Knight Foundation Bill Keller afternoon paper I love the Weather & Opera section bloggers in their mother's basement retweet analog thinking crowdsourcing, just across the wire David Cohn mthomps </p>
             </div> 
         </div>
     )
